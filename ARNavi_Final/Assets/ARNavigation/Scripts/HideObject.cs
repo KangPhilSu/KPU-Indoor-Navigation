0ad6100f9c41b51eb9dd7cdcde3f1422b3c84f49
VespertinoLuna <58764766+VespertinoLuna@users.noreply.github.com>
2020-08-23 17:20:49 +0900

    Revert "나침반 및 남은거리 표시"
    This reverts commit 603403b766a9cb29b538b2b69ecb27a6ab3c38c6.

---
+++ ARNavi_Final/Assets/ARNavigation/Scripts/HideObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour
+{
+
+    public Transform Agent;
+    /*public Transform mt;
+    public GameObject me;*/
+
+    //float dist = Vector3.Distance(Distancetool1.transform.position, Distancetool2.transform.position);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // float dist = Vector3.Distance(Distancetool1.position, Distancetool2.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //float dist = Vector3.Distance(Distancetool1.transform.position, Distancetool2.transform.position);
+        float DtoA = Vector3.Distance(this.transform.position, Agent.transform.position);
+
+        if((int)DtoA > 5)
+        {
+            this.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            this.GetComponent<MeshRenderer>().enabled = true;
+        }
+        
+    }
+}
